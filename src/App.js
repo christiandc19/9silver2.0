3038159080e291ad8122ea92423c6e0aed547b2a
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-09 10:56:11 -0800

    initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './nav-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Comming soon</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
